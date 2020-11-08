0f895b9333f56846a6de6f13bff09f1a31aeb727
evkrem <evkremx@gmail.com>
2020-11-08 21:38:30 +0200

    добавляем изменения в тесты

--- test_items.py
+++ test_items.py
@@ -8,6 +8,18 @@ def test_guest_should_see_login_link(browser,language):
     lang = language  #присваиваем переменной lang
     link = f"http://selenium1py.pythonanywhere.com/{lang}/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(2)
-    browser.find_element_by_css_selector("#add_to_basket_form button").click()
-    time.sleep(10)
+    selector = "#add_to_basket_form button"
+    time.sleep(1)
+    def is_element_present(browser):
+        try:
+            browser.implicitly_wait(2)
+            browser.find_element_by_css_selector(selector)
+            return True
+        except:
+            return False
+    # z = is_element_present(browser)
+    # print("zzzzzz= ",z)
+    # assert z == True, "корзинка не найдена"
+    assert is_element_present(browser) == True, "корзинка не найдена"
+    browser.find_element_by_css_selector(selector).click()
+    time.sleep(1)
